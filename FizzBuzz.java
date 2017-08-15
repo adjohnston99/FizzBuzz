ed30c730d6577f70e309cf26632c4c430d9979e5
Andrew Johnston <adjohnston99@gmail.com>
2017-08-14 23:19:29 -0700

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+	 public static void main(String[] args) {
+		
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.print("Enter your test numbers seperated by commas: ");
+		 String input = scanner.next();
+		 String[] inputSplit = input.split(",");
+		 for(int i = -1; inputSplit[i] != null; i++) {
+			 System.out.println(inputSplit[i]);
+		 }
+		 
+		 int[] testNums = {3, 5, 7, 11, 13};
+		 String[] responseStrings = {"Fizz", "Buzz", "Fuzz", "Bizz", "Biff"};
+		 String output;
+		 
+		 for(int i = 1; i <= 1000; i++) {
+			 output = "";
+			 for(int j = 0; j < testNums.length; j++) {
+				 if(i % testNums[j] == 0) {
+					 output += responseStrings[j];
+				 }
+			 }
+			 if(output != "") {
+				 System.out.println(output);
+			 } else {
+				 System.out.println(i);
+			 }
+		 }
+	 }
+}
